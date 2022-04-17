a62a3c2b896a145cc6bf126f4b93cc0cca2d0266
Reginaldo de Araujo Oliveira <rao.corretor@gmail.com>
2022-04-17 08:30:01 -0300

    2º Dia - HTML/CSS/JS
    Esquematização inicial das abas:
    Cadastro Clientes;
    Cadastro Produtos.

--- produtos.js
+++ produtos.js
@@ -1,41 +1,99 @@
-var modal = document.getElementById('formulario');
+var salvar = new Array();
 
-var btn = document.getElementsByClassName('add_table')[0];
+class Cadastro{
+  constructor(nome,cpf, sexo, email, telefone, nascimento, cidade, estado ){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.sexo = sexo;
+    this.email = email;
+    this.telefone = telefone;
+    this.nascimento = nascimento;
+    this.cidade = cidade;
+    this.estado = estado;
+  }
 
-var close = document.getElementsByClassName("close")[0];
-
-var edit = document.getElementsByClassName("fa fa-pencil-square-o");
-
-var input = document.getElementsByTagName("input");
-
-var option = document.getElementsByTagName("option");
-
-function clearContents(){
-    modal.style.top = "-200%";
-    modal.style.background = "rgba(0,0,0,0.0)";
-    modal.style.zIndex = -1;
-    document.getElementById('title-form').innerHTML = "Cadastro de novo Usuário";
-    document.getElementsByName("action")[0].value = "create";
-    for(x=0;x<input.length-1;x++){
-        input[x].value = "";
+  mensagem()
+    {
+    alert("Cadastro Efetuado!"+"\n"+"\n"+this.nome+"\n"+this.cpf+"\n"+this.sexo+"\n"+this.email+"\n"+this.telefone+"\n"+this.nascimento+"\n"+this.cidade+"\n"+this.estado);
     }
-    for(x=0;x<option.length;x++){
-    	option[x].selected = false;
-    }
- }
 
-btn.onclick = function() {
-    modal.style.zIndex = 1;modal.style.top = 0;
-    modal.style.background = "rgba(0,0,0,0.8)";
 }
 
-close.onclick = function() {
-	clearContents();
+
+/* LISTAGEM DOS CADASTROS NA .HOME*/
+function carregar(){
+  var carrega = document.getElementById('load');
+  var tamanho = salvar.length;
+  var texto = "";
+  if(tamanho !=0){
+    texto="";
+    for(i=0;i<tamanho;i++)
+    {
+     texto = texto+ '<li onclick="mostrar(' + salvar[i].cpf + ')">' + salvar[i].nome+"</li>";
+     }
+  } 				
+  else{
+    texto = "Nenhum cadastro até o momento";
+  }
+
+  carrega.innerHTML = texto;
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        clearContents();
-    }
+function mostrar(cpf){
+  alert(cpf);
+}
 
+
+/* LIMPAR AS INPUTS E SELECTS PARA PODER DIGITAR DE NUEVO */
+function limpar(){
+  document.querySelector('#nome').value= '';
+  document.querySelector('#cpf').value= '';
+  document.querySelector('#sexo').selectedIndex = 0;
+  document.querySelector('#email').value= '';
+  document.querySelector('#telefone').value= '';
+  document.querySelector('#nascimento').selectedIndex = 0;
+  document.querySelector('#cidade').value = '';
+  document.querySelector('#estado').selectedIndex = 0;
+
+}
+
+
+/* SALVAR OS CADASTROS REALIZADOS*/
+document.querySelector('#btnsalvar').onclick = function(evt){
+
+  var nome = document.querySelector('#nome').value;
+  var cpf = document.querySelector('#cpf').value;
+  var sexo = document.querySelector('#sexo').value;
+  var email = document.querySelector('#email').value;
+  var telefone = document.querySelector('#telefone').value;
+  var nascimento = document.querySelector('#nascimento').value;
+  var cidade = document.querySelector('#cidade').value;
+  var estado = document.querySelector('#estado').value;
+  var usuario = new Cadastro(nome,cpf,sexo,email,telefone,nascimento,cidade,estado);
+  salvar.push(usuario);
+  usuario.mensagem();
+  carregar();	
+
+  limpar();
+
+}
+
+
+/* INFORMAÇÕES DO ICONE INFO */
+document.querySelector('#info').onclick = function(evt){
+  alert("Cadastro de Clientes");
+}
+
+
+/* FAZER A .HOME E A .CADASTRO SUMIR E APARECER*/
+document.querySelector('.cadastro').style.display = 'none'
+
+document.querySelector('#add').onclick = function(evt){
+  document.querySelector('.cadastro').style.display = 'block'
+  document.querySelector('.home').style.display = 'none'
+}
+
+document.querySelector('#voltar').onclick = function(evt){
+  document.querySelector('.cadastro').style.display = 'none'
+  document.querySelector('.home').style.display = 'block'
 }
